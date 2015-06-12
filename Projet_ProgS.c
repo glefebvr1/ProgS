c3f0b2cda3b10417b6fb53d62d61a45d0fc208f2
glefebvr1 <guillaume@lefebvre.li>
2015-06-12 11:37:19 +0200

    creation projet

---
+++ Projet_ProgS.c
@@ -0,0 +1,127 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_CHAINE 50
+#define VRAI 1
+#define FAUX 0
+
+//structures
+
+typedef struct {
+	int no;
+	char *marque;
+	char *ref;
+	float prix_unitaire;
+}type_produit;
+
+struct{
+	type_produit *ptr_produit;
+	int quantite;
+	float total_ligne;
+}type_ligne_commande;
+
+
+//Saisie d'une chaine et 
+void Saisie_Chaine(char chaine[MAX_CHAINE]);
+
+int Saisie_Entier();
+
+void Afficher_Ligne_Commande(type_ligne_commande ligne_commande);
+
+//Recherche dans le tableau de commande si un n° de produit existe déjà
+type_ligne_commande *Recherche_Ligne(int no, int nb_ligne_commande, type_ligne_commande *tab_commande);
+
+//Recherche dans le tableau de produits si un n° de produit existe déjà. Retourne l'adresse du produit correspondant ou NULL si ce produit n'existe pas.
+type_produit *Recherche_Produit(int no, int nb_ligne_produit, type_produit *tab_produit);
+
+void Creation_Ligne(type_produit *tab_produit, type_ligne_commande *tab_commande, int nb_ligne_commande);
+
+void Modif_Ligne(type_ligne_commande *tab_commande,int nv_quantite);
+
+void Commande_Produit(type_produit *tab_produit, type_ligne_commande *tab_commande, int nb_ligne_commande, int nb_ligne_produit);
+
+void Supprimer_ligne(type_ligne_commande *tab_commande, int nb_ligne_commande, int no);
+
+void Creation_Facture(char *nom, char *prenom, float total, type_ligne_commande *tab_commande, int nb_ligne_commande);
+
+void main(){
+
+	char *nom;
+	char *prenom;
+	float total;
+	int nb_ligne_commande
+	type_ligne_commande *tab_commande;
+	int nb_ligne_produit
+	type_produit *tab_produit;
+	char chaine_tmp[MAX_CHAINE];
+
+
+
+
+
+}
+
+
+//Saisie d'une chaine et 
+void Saisie_Chaine(char chaine[MAX_CHAINE]){
+
+
+	
+}
+
+int Saisie_Entier(){
+
+
+	
+}
+
+void Afficher_Ligne_Commande(type_ligne_commande ligne_commande){
+
+
+	
+}
+
+//Recherche dans le tableau de commande si un n° de produit existe déjà
+type_ligne_commande *Recherche_Ligne(int no, int nb_ligne_commande, type_ligne_commande *tab_commande){
+
+
+	
+}
+
+//Recherche dans le tableau de produits si un n° de produit existe déjà. Retourne l'adresse du produit correspondant ou NULL si ce produit n'existe pas.
+type_produit *Recherche_Produit(int no, int nb_ligne_produit, type_produit *tab_produit){
+
+
+	
+}
+
+void Creation_Ligne(type_produit *tab_produit, type_ligne_commande *tab_commande, int nb_ligne_commande){
+
+
+	
+}
+
+void Modif_Ligne(type_ligne_commande *tab_commande,int nv_quantite){
+
+
+	
+}
+
+void Commande_Produit(type_produit *tab_produit, type_ligne_commande *tab_commande, int nb_ligne_commande, int nb_ligne_produit){
+
+
+	
+}
+
+void Supprimer_ligne(type_ligne_commande *tab_commande, int nb_ligne_commande, int no){
+
+
+	
+}
+
+void Creation_Facture(char *nom, char *prenom, float total, type_ligne_commande *tab_commande, int nb_ligne_commande){
+
+
+	
+}
