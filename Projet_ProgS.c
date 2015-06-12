7dd6f5b8497b4f553fbbc96d44223b573897806e
glefebvr1 <guillaume@lefebvre.li>
2015-06-12 21:08:13 +0200

    charge_produit

--- Projet_ProgS.c
+++ Projet_ProgS.c
@@ -135,6 +135,51 @@ type_produit *Recherche_Produit(int no, int nb_ligne_produit, type_produit *tab_
 	
 }
 
+// Charge le fichier produit et l'enregistre dans un tableau de structures
+type_produit *Charge_Produits(char chemin_fichier[MAX_CHAINE], int *nb_produits) {
+	FILE *fichier_produit;
+	char ligne[MAX_CHAINE], marque[MAX_CHAINE], ref[MAX_CHAINE];
+	type_produit *tab_produit, tab_produit_tmp[MAX_PRODUITS];
+	int ret, i;
+	bool erreur;
+
+	erreur = FAUX;
+	fichier_produit = fopen(chemin_fichier, "r");
+
+	if (fichier_produit == NULL) {
+		puts("Le fichier n'existe pas ou n'est pas accessible");
+		erreur = VRAI;
+	}
+	else {
+		while (fgets(ligne, MAX_CHAINE, fichier_produit) != NULL && !erreur) {
+			ret = sscanf(ligne, "%d\t%s\t%s\t%f", &tab_produit_tmp[*nb_produits].no, marque, ref, &tab_produit_tmp[*nb_produits].prix_unitaire);
+			if (ret != 4) {
+				printf("Le format de la ligne %d est incorrect\n", *nb_produits + 1);
+				erreur = VRAI;
+			}
+			else {
+				tab_produit_tmp[*nb_produits].marque = (char *)malloc(strlen(marque) * sizeof(char));
+				tab_produit_tmp[*nb_produits].ref = (char *)malloc(strlen(ref) * sizeof(char));
+				strcpy(tab_produit_tmp[*nb_produits].marque, marque);
+				strcpy(tab_produit_tmp[*nb_produits].ref, ref);
+				(*nb_produits)++;
+			}
+		}
+
+		tab_produit = (type_produit *)malloc(*nb_produits * sizeof(type_produit));
+		for (i = 0; i < *nb_produits; i++) {
+			tab_produit[i] = tab_produit_tmp[i];
+		}
+	}
+
+	if (erreur) {
+		return NULL;
+	}
+	else {
+		return tab_produit;
+	}
+}
+
 void Creation_Ligne(type_produit *tab_produit, type_ligne_commande *tab_commande, int nb_ligne_commande){
 
 
