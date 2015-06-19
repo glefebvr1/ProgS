975d2dc59d21dd5c45321834a48a259c2d9418b8
glefebvr1 <guillaume@lefebvre.li>
2015-06-19 14:29:45 +0200

    Commentaires

--- Projet_ProgS.c
+++ Projet_ProgS.c
@@ -36,6 +36,8 @@ void Afficher_Ligne_Commande(type_ligne_commande *ligne_commande);
 //Recherche dans le tableau de commande si un n° de produit existe déjà
 type_ligne_commande *Recherche_Ligne(int no, type_ligne_commande *tab_commande, type_ligne_commande *derniere_ligne);
 
+type_produit Verifie_Ligne_Produit(char ligne[], int no_ligne);
+
 // Charge le fichier produit et l'enregistre dans un tableau de structures
 type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_produit);
 
@@ -62,15 +64,16 @@ void main() {
 	type_ligne_commande *tab_commande, *ligne_commande, *derniere_ligne;
 	type_produit *tab_produit, *dernier_produit, *produit_courant;
 
+	init_nom = FAUX;
+	total = 0;
+	tab_commande = NULL;
+	derniere_ligne = NULL;
 	dernier_produit = NULL;
 
 	tab_produit = Charge_Produits(FPRODUIT, &dernier_produit);
 
+	// On quitte si on arrive pas à charger les produits
 	if (tab_produit != NULL) {
-		init_nom = FAUX;
-		total = 0;
-		tab_commande = NULL;
-		derniere_ligne = NULL;
 
 		//Option
 		puts("1. Saisir le nom et le prenom du client.");
@@ -120,6 +123,7 @@ void main() {
 			choix = Saisie_Entier();
 		}
 
+		// Libération de la mémoire
 		if (init_nom) {
 			free(nom);
 			free(prenom);
@@ -200,13 +204,17 @@ int Saisie_Entier(){
 	return valeur;
 }
 
+// Affichage d'une ligne de commande uniquement si elle est définie
 void Afficher_Ligne_Commande(type_ligne_commande *ligne_commande) {
 	if (ligne_commande != NULL) {
 		printf("Commande de %d %s %s, prix unitaire : %.2f CHF, total : %.2f CHF.\n", ligne_commande->quantite, ligne_commande->ptr_produit->marque, ligne_commande->ptr_produit->ref, ligne_commande->ptr_produit->prix_unitaire, ligne_commande->total_ligne);
 	}
 }
 
-//Recherche dans le tableau de commande si un n° de produit existe déjà
+// Recherche dans le tableau de commande si une ligne de commande existe déjà pour
+//	le no de produit donné
+// Renvoie NULL si aucune ligne de commande n'existe pour le no de produit donné,
+//	sinon renvoie un pointeur vers la ligne contenant le produit
 // Tests :
 // - n'importe quelle valeur de no
 // - tab_commande NULL
@@ -216,7 +224,8 @@ type_ligne_commande *Recherche_Ligne(int no, type_ligne_commande *tab_commande, 
 
 	resultat = NULL;
 
-	if (tab_commande != NULL) {
+	// Pas de recherche s'il n'existe aucune commande
+	if (tab_commande != NULL && derniere_ligne != NULL) {
 		element_courant = tab_commande;
 
 		while (element_courant <= derniere_ligne && resultat == NULL) {
@@ -230,7 +239,8 @@ type_ligne_commande *Recherche_Ligne(int no, type_ligne_commande *tab_commande, 
 	return resultat;
 }
 
-//Recherche dans le tableau de produits si un n° de produit existe déjà. Retourne l'adresse du produit correspondant ou NULL si ce produit n'existe pas.
+//Recherche dans le tableau de produits si un n° de produit existe déjà.
+// Retourne un pointeur vers le produit correspondant ou NULL si ce produit n'existe pas.
 // Tests :
 // - n'importe quelle valeur de no
 // - tab_produit NULL
@@ -240,6 +250,7 @@ type_produit *Recherche_Produit(int no, type_produit *tab_produit, type_produit 
 
 	resultat = NULL;
 
+	// Pas de recherche s'il n'existe aucun produit
 	if (tab_produit != NULL && dernier_produit != NULL) {
 		element_courant = tab_produit;
 
@@ -253,7 +264,10 @@ type_produit *Recherche_Produit(int no, type_produit *tab_produit, type_produit 
 	return resultat;
 }
 
-type_produit Ajoute_Ligne_Produit(char ligne[], int no_ligne) {
+// Vérifie si une ligne du fichier produit ne contient pas d'erreur
+//	Retourne le produit extrait de la ligne  si aucune erreur,
+//	sinon retourne un produit "vide" dont le no vaut -1
+type_produit Verifie_Ligne_Produit(char ligne[], int no_ligne) {
 	int ret, erreur, no;
 	float prix;
 	char marque[MAX_CHAINE], ref[MAX_CHAINE];
@@ -294,7 +308,11 @@ type_produit Ajoute_Ligne_Produit(char ligne[], int no_ligne) {
 	return produit;
 }
 
-// Charge le fichier produit et l'enregistre dans un tableau de structures
+// Charge le fichier produit et l'enregistre dans un tableau de structures.
+// Renvoie un pointeur vers le tableau de structures si le chargement à réussi,
+//	sinon renvoie NULL
+// Toute erreur lors du chargement du fichier est considérée comme fatale et
+//	entraîne l'annulation du chargement
 // Tests
 // - ligne incomplète
 // - ligne vide
@@ -322,6 +340,8 @@ type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_prod
 		erreur = VRAI;
 	}
 	else {
+		// Une fois le fichier ouvert, on récupère chaque ligne du fichier tant
+		// qu'il existe des lignes
 		ret = fgets(ligne, MAX_CHAINE, fichier_produit);
 		while (ret != NULL && !erreur) {
 			if (no_ligne >= MAX_PRODUITS) {
@@ -329,8 +349,9 @@ type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_prod
 				erreur = VRAI;
 			}
 			else {
-				produit = Ajoute_Ligne_Produit(ligne, no_ligne);
-
+				// On vérifie chaque ligne, puis si la ligne est ok, on ajoute le
+				// produit correspondant au tableau tab_produit_tmp
+				produit = Verifie_Ligne_Produit(ligne, no_ligne);
 				if (produit.no != -1) {
 					if (Recherche_Produit(produit.no, tab_produit_tmp, *dernier_produit) != NULL) {
 						printf("La ligne %d contient un doublon\n", no_ligne + 1);
@@ -359,8 +380,9 @@ type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_prod
 		return NULL;
 	}
 	else {
+		// On copie la tableau statique des produits chargés dans un tableau
+		// dynamique
 		tab_produit = (type_produit *)malloc(no_ligne * sizeof(type_produit));
-
 		for (i = 0; i < no_ligne; i++) {
 			tab_produit[i] = tab_produit_tmp[i];
 		}
@@ -371,17 +393,20 @@ type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_prod
 	}
 }
 
+// Ajoute une ligne de commande au tableau des commandes, recalcule le total de la commande
+// et incrémente le pointeur de dernière ligne de commande
 // Tests
 // - quantité <= 0
 // - pas de ligne
 void Ajout_Ligne(type_ligne_commande **tab_commande, type_ligne_commande **derniere_ligne, int nb_produits, type_produit *nv_ptr_produit, int quantite, float *total) {
 
 	if (quantite > 0) {
-		//Ajout des valeurs quantité et total dans le tableau commande
+		// Préparation pour insertion de la 1ère ligne
 		if (*tab_commande == NULL) {
 			*tab_commande = (type_ligne_commande *)malloc(sizeof(type_ligne_commande) * nb_produits);
 			*derniere_ligne = *tab_commande;
 		}
+		// Préparation pour insertion des autres lignes
 		else {
 			(*derniere_ligne)++;
 		}
@@ -390,7 +415,7 @@ void Ajout_Ligne(type_ligne_commande **tab_commande, type_ligne_commande **derni
 		(*derniere_ligne)->total_ligne = quantite * nv_ptr_produit->prix_unitaire;
 		*total = *total + (*derniere_ligne)->total_ligne;
 
-		//Affichage du resultat
+		//Affichage de la nouvelle ligne de commande
 		Afficher_Ligne_Commande(*derniere_ligne);
 	}
 	else {
@@ -398,36 +423,43 @@ void Ajout_Ligne(type_ligne_commande **tab_commande, type_ligne_commande **derni
 	}
 }
 
+// Modifie une ligne de commande du tableau des commandes et recalcule le total de la commande
 // Tests
 // - empecher quantite negatives
 void Modif_Ligne(int nv_quantite, type_ligne_commande *ligne_commande, float *total) {
 
 	if (nv_quantite + ligne_commande->quantite > 0) {
-		//modification des valeurs quantité et total dans le tableau commande
 		ligne_commande->quantite = nv_quantite + ligne_commande->quantite;
 		ligne_commande->total_ligne = ligne_commande->quantite * ligne_commande->ptr_produit->prix_unitaire;
-		Afficher_Ligne_Commande(ligne_commande);
-		//modification du total
+
 		*total = *total + nv_quantite * ligne_commande->ptr_produit->prix_unitaire;
+
+		//Affichage de la ligne de commande modifiée
+		Afficher_Ligne_Commande(ligne_commande);
 	}
 	else {
 		puts("Il n'est pas possible de commander une quantite inferieure a 1");
 	}
 }
 
+// Supprime une ligne de commande du tableau des commandes, recalcule le total de la commande,
+//	décale les éléments du tableau situés à droite de l'élément supprimé d'un cran à gauche et
+//	décrémente le pointeur de dernière ligne de commande
 // Tests
 // - ligne commande vide
 void Supprimer_ligne(type_ligne_commande **tab_commande, type_ligne_commande *adresse_commande, type_ligne_commande **derniere_ligne, float *total) {
 
 	if (*tab_commande != NULL) {
-		//modification du total
+
 		*total = *total - (adresse_commande->total_ligne);
 
+		// décalage des éléments
 		while (adresse_commande < *derniere_ligne) {
 			*adresse_commande = *(adresse_commande + 1);
 			adresse_commande++;
 		}
 
+		// Si le dernier élément est supprimé, libération de la mémoire
 		if (*derniere_ligne == *tab_commande) {
 			free(*tab_commande);
 			*tab_commande = NULL;
@@ -438,13 +470,13 @@ void Supprimer_ligne(type_ligne_commande **tab_commande, type_ligne_commande *ad
 	}
 }
 
+// Gestion de l'ajout, modification ou suppression d'une ligne de commande
 void Commande_Produit(type_produit *tab_produit, type_ligne_commande **tab_commande, type_ligne_commande **derniere_ligne, type_produit *dernier_produit, float *total) {
 
 	int no_produit, quantite, nb_produits;
 	type_produit *adresse_produit;
 	type_ligne_commande *adresse_commande;
 
-	//Demande du numéro de produit
 	printf("Entrez le numero de produit : ");
 	no_produit = Saisie_Entier();
 	while (no_produit < 0){
@@ -452,21 +484,23 @@ void Commande_Produit(type_produit *tab_produit, type_ligne_commande **tab_comma
 		no_produit = Saisie_Entier();
 	}
 
-	//On recherche si le produit existe.
+	// On recherche si le produit existe.
 	adresse_produit = Recherche_Produit(no_produit, tab_produit, dernier_produit);
 	if (adresse_produit == NULL) {
 		puts("Commande impossible car le produit n'existe pas.");
 	}
 	else {
-		//Demande la quantité
 		printf("Entrez la quantite desiree : ");
 		quantite = Saisie_Entier();
 
+		// On recherche si une ligne de commande contient déjà ce produit
 		adresse_commande = Recherche_Ligne(no_produit, *tab_commande, *derniere_ligne);
+		// Si non, on ajoute une ligne
 		if (adresse_commande == NULL) {
 			nb_produits = dernier_produit - tab_produit + 1;
 			Ajout_Ligne(tab_commande, derniere_ligne, nb_produits, adresse_produit, quantite, total);
 		}
+		// Si oui, on modifie ou on supprime une ligne selon la quantité
 		else {
 			if (quantite == 0) {
 				Supprimer_ligne(tab_commande, adresse_commande, derniere_ligne, total);
@@ -478,6 +512,7 @@ void Commande_Produit(type_produit *tab_produit, type_ligne_commande **tab_comma
 	}
 }
 
+// Génération du fichier HTML de la facture
 // Tests
 // - fichier existe, pas de droits
 // - pas possible d'enregistrer modif
@@ -486,6 +521,7 @@ void Creation_Facture(char *nom, char *prenom, float total, type_ligne_commande 
 	FILE *fichier_facture;
 	char nom_complet[MAX_CHAINE];
 
+	// On nomme le fichier d'après le nom et prénom de la personne
 	strcpy(nom_complet, nom);
 	strcat(nom_complet, prenom);
 	fichier_facture = fopen(strcat(nom_complet, ".html"), "w");
@@ -498,10 +534,12 @@ void Creation_Facture(char *nom, char *prenom, float total, type_ligne_commande 
 		fprintf(fichier_facture, "<h1>Facture de %s %s</h1>\n", nom, prenom);
 		fputs("<table border>\n<tr bgcolor=\"yellow\">", fichier_facture);
 		fputs("<td>No</td><td>Marque</td><td>Ref</td><td>Prix</td><td>Nb</td><td>Total</td></tr>\n", fichier_facture);
+		// On imprime chaque ligne de commande
 		while (tab_commande <= derniere_ligne) {
 			fprintf(fichier_facture, "<tr><td>%d</td><td>%s</td><td>%s</td><td>%.2f</td><td>%d</td><td align=\"right\">%.2f</td></tr>\n", tab_commande->ptr_produit->no, tab_commande->ptr_produit->marque, tab_commande->ptr_produit->ref, tab_commande->ptr_produit->prix_unitaire, tab_commande->quantite, tab_commande->total_ligne);
 			tab_commande++;
 		}
+		// On imprime le total de la commande
 		fprintf(fichier_facture, "<tr><td>Total</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td align=\"right\">%.2f</td></tr>\n", total);
 		fputs("</table>\n</body>\n</html>", fichier_facture);
 
