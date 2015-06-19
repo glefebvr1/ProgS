d2e8bd7d5811a965aa78a71ce01edc34e59b2740
glefebvr1 <guillaume@lefebvre.li>
2015-06-19 17:41:29 +0200

    Gestion erreurs

--- Projet_ProgS.c
+++ Projet_ProgS.c
@@ -2,16 +2,24 @@
 #include <stdio.h>
 #include <string.h>
 
+// Longueur max des entiers saisis (sans \n)
 #define MAX_INT 10
+// Longueur max d'une ligne d'un fichier (sans \n)
+#define MAX_LIGNE 255
+// Longueur max des chaînes de caractères
 #define MAX_CHAINE 255
+// Longueur max des chaînes saisies par l'utilisateur (sans \n)
 #define MAX_CHAINE_SAISIE 50
+// Nombre max de produits que le fichier FPRODUIT peut contenir
 #define MAX_PRODUITS 255
-#define VRAI 1
-#define FAUX 0
+// Fichier contenant la liste des produits
 #define FPRODUIT "produit.txt"
 
-//structures
+#define VRAI 1
+#define FAUX 0
 
+// La structure type_produit contient toutes les informations de chaque produit
+//	chargé depuis le fichier
 typedef struct {
 	int no;
 	char *marque;
@@ -19,6 +27,9 @@ typedef struct {
 	float prix_unitaire;
 } type_produit;
 
+// La structure type_ligne_commande contient toutes les informations de chaque
+//	ligne de la commande. Un pointeur est utilisé pour référencer le produit
+//	commandé
 typedef struct {
 	type_produit *ptr_produit;
 	int quantite;
@@ -70,6 +81,7 @@ void main() {
 	derniere_ligne = NULL;
 	dernier_produit = NULL;
 
+	// Chargement des produits en mémoire depuis le fichier
 	tab_produit = Charge_Produits(FPRODUIT, &dernier_produit);
 
 	// On quitte si on arrive pas à charger les produits
@@ -86,32 +98,40 @@ void main() {
 		choix = Saisie_Entier();
 		while (choix != 0) {
 			switch (choix) {
+			// Saisie du nom et prénom
 			case 1:
-				//Saisie du nom
 				puts("Entrez le nom du client.");
 				nom = Saisie_Chaine();
 
-				//Saisie du prénom
 				puts("Entrez le prenom du client.");
 				prenom = Saisie_Chaine();
 
 				init_nom = VRAI;
 				break;
+			// Ajout, modification ou suppression de la commande d'un produit
 			case 2:
-				if (init_nom){
+				if (init_nom) {
 					Commande_Produit(tab_produit, &tab_commande, &derniere_ligne, dernier_produit, &total);
 				} else {
 					puts("Vous ne pouvez pas faire de commande tant que les nom et prenom n'ont pas ete entre.");
 				}
 				break;
+			// Affichage de la commande
 			case 3:
-				ligne_commande = tab_commande;
-				while (ligne_commande <= derniere_ligne) {
-					Afficher_Ligne_Commande(ligne_commande);
-					ligne_commande++;
+				if (tab_commande == NULL) {
+					puts("Aucune commande");
 				}
-				printf("Total : %.2f\n", total);
+				else {
+					// On affiche chaque ligne de la commande
+					ligne_commande = tab_commande;
+					while (ligne_commande <= derniere_ligne) {
+						Afficher_Ligne_Commande(ligne_commande);
+						ligne_commande++;
+					}
+					printf("Total : %.2f\n", total);
+				}
 				break;
+			// Génération de la facture HTML
 			case 4:
 				if (init_nom) {
 					Creation_Facture(nom, prenom, total, tab_commande, derniere_ligne);
@@ -132,9 +152,11 @@ void main() {
 			free(nom);
 			free(prenom);
 		}
+
 		if (tab_commande != NULL) {
 			free(tab_commande);
 		}
+
 		produit_courant = tab_produit;
 		while (produit_courant <= dernier_produit) {
 			free(produit_courant->marque);
@@ -147,14 +169,18 @@ void main() {
 	system("pause");
 }
 
+// Vérifie qu'une chaîne saisie soit correcte : pas de chaine vide ni de
+//	chaîne trop longue. Pas de caractères spéciaux.
+// Retourne la chaîne de caractère saisie par l'utilisateur
 // Tests :
 // - Saisie d'une chaine trop longue
 // - allocation mémoire pour strlen + 1
 // - saisie d'une chaine vide (\n)
 char *Saisie_Chaine(){
-	char chaine_tmp[MAX_CHAINE_SAISIE], *chaine;
+	char chaine_tmp[MAX_CHAINE_SAISIE + 2], *chaine;
 
-	fgets(chaine_tmp, MAX_CHAINE_SAISIE, stdin);
+	// Tant que la chaîne saisie est incorrecte, on redemande la saisie
+	fgets(chaine_tmp, MAX_CHAINE_SAISIE + 2, stdin);
 	while (chaine_tmp[0] == '\n' || chaine_tmp[strlen(chaine_tmp) - 1] != '\n') {
 		if (chaine_tmp[strlen(chaine_tmp) - 1] != '\n') {
 			while (getchar() != '\n');
@@ -163,7 +189,7 @@ char *Saisie_Chaine(){
 		else {
 			puts("Saisie invalide");
 		}
-		fgets(chaine_tmp, MAX_CHAINE_SAISIE, stdin);
+		fgets(chaine_tmp, MAX_CHAINE_SAISIE + 2, stdin);
 	}
 
 	//suppression du '\n'
@@ -171,6 +197,7 @@ char *Saisie_Chaine(){
 		chaine_tmp[strlen(chaine_tmp) - 1] = '\0';
 	}
 
+	// Copie de la chaîne dans un tableau dynamique
 	chaine = (char *)malloc(sizeof(char) * strlen(chaine_tmp) + 1);
 	strcpy(chaine, chaine_tmp);
 
@@ -183,15 +210,15 @@ char *Saisie_Chaine(){
 // - Saisie trop longue
 // - Saisie de valeur non entières
 int Saisie_Entier(){
-
-	char entier[MAX_INT];
+	char entier[MAX_INT + 2];
 	int ret, valeur;
 
-	// lit un entier de max MAX_INT caractères
 	printf("> ");
-	fgets(entier, MAX_INT, stdin);
+	// lit un entier de max MAX_INT caractères
+	fgets(entier, MAX_INT + 2, stdin);
 	ret = sscanf(entier, "%d", &valeur);
 
+	// Tant que le nombre saisi est incorrect, on redemande la saisie
 	while (ret != 1 || entier[strlen(entier) - 1] != '\n') {
 		if (entier[strlen(entier) - 1] != '\n') {
 			while (getchar() != '\n');
@@ -201,7 +228,7 @@ int Saisie_Entier(){
 			puts("Saisie invalide");
 		}
 		printf("> ");
-		fgets(entier, MAX_INT, stdin);
+		fgets(entier, MAX_INT + 2, stdin);
 		ret = sscanf(entier, "%d", &valeur);
 	}
 
@@ -239,6 +266,9 @@ type_ligne_commande *Recherche_Ligne(int no, type_ligne_commande *tab_commande, 
 			element_courant++;
 		}
 	}
+	else {
+		puts("Erreur : tableau de commande mal defini");
+	}
 
 	return resultat;
 }
@@ -265,6 +295,9 @@ type_produit *Recherche_Produit(int no, type_produit *tab_produit, type_produit 
 			element_courant++;
 		}
 	}
+	else {
+		puts("Erreur : Tableau des produits mal defini");
+	}
 	return resultat;
 }
 
@@ -279,7 +312,7 @@ type_produit Verifie_Ligne_Produit(char ligne[], int no_ligne) {
 
 	erreur = FAUX;
 
-	if (ligne[strlen(ligne) - 1] != '\n' && strlen(ligne) == MAX_CHAINE - 1) {
+	if (ligne[strlen(ligne) - 1] != '\n' && strlen(ligne) >= MAX_LIGNE) {
 		printf("Nombre de caracteres trop grand sur la ligne %d\n", no_ligne + 1);
 		erreur = VRAI;
 	}
@@ -322,15 +355,16 @@ type_produit Verifie_Ligne_Produit(char ligne[], int no_ligne) {
 // - ligne vide
 // - valeur incorrecte dans champ
 // - no produit < 0
-// - fichier inexistant ou pas de droits
+// - fichier inexistant
+// - pas de droits sur le fichier
 // - nombre de produits trop élevés
 // - erreur de fermeture du fichier
 // - longueur de ligne trop grande dans fichier
 // - gestion des doublons
-// A TESTER - fichier vide
+// - fichier vide
 type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_produit) {
 	FILE *fichier_produit;
-	char ligne[MAX_CHAINE], *ret;
+	char ligne[MAX_LIGNE + 2], *ret;
 	type_produit *tab_produit, tab_produit_tmp[MAX_PRODUITS], produit;
 	int i, no_ligne;
 	int erreur;
@@ -346,7 +380,7 @@ type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_prod
 	else {
 		// Une fois le fichier ouvert, on récupère chaque ligne du fichier tant
 		// qu'il existe des lignes
-		ret = fgets(ligne, MAX_CHAINE, fichier_produit);
+		ret = fgets(ligne, MAX_LIGNE + 2, fichier_produit);
 		while (ret != NULL && !erreur) {
 			if (no_ligne >= MAX_PRODUITS) {
 				printf("Le fichier %s contient trop de produits\n", chemin_fichier);
@@ -363,11 +397,15 @@ type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_prod
 					}
 					else {
 						tab_produit_tmp[no_ligne] = produit;
+						*dernier_produit = &tab_produit_tmp[no_ligne];
 					}
+				}
+				else {
+					erreur = VRAI;
 				}
 				no_ligne++;
 			}
-			ret = fgets(ligne, MAX_CHAINE, fichier_produit);
+			ret = fgets(ligne, MAX_LIGNE + 2, fichier_produit);
 		}
 		if (fclose(fichier_produit) == EOF) {
 			printf("Erreur de fermeture du fichier %s\n", chemin_fichier);
@@ -404,26 +442,31 @@ type_produit *Charge_Produits(char chemin_fichier[], type_produit **dernier_prod
 // - pas de ligne
 void Ajout_Ligne(type_ligne_commande **tab_commande, type_ligne_commande **derniere_ligne, int nb_produits, type_produit *nv_ptr_produit, int quantite, float *total) {
 
-	if (quantite > 0) {
-		// Préparation pour insertion de la 1ère ligne
-		if (*tab_commande == NULL) {
-			*tab_commande = (type_ligne_commande *)malloc(sizeof(type_ligne_commande) * nb_produits);
-			*derniere_ligne = *tab_commande;
-		}
-		// Préparation pour insertion des autres lignes
-		else {
-			(*derniere_ligne)++;
-		}
-		(*derniere_ligne)->ptr_produit = nv_ptr_produit;
-		(*derniere_ligne)->quantite = quantite;
-		(*derniere_ligne)->total_ligne = quantite * nv_ptr_produit->prix_unitaire;
-		*total = *total + (*derniere_ligne)->total_ligne;
-
-		//Affichage de la nouvelle ligne de commande
-		Afficher_Ligne_Commande(*derniere_ligne);
+	if (nb_produits <= 0 || *derniere_ligne == NULL || nv_ptr_produit == NULL) {
+		puts("Erreur d'adressage");
 	}
 	else {
-		puts("La quantite saisie ne peut etre <= 0");
+		if (quantite > 0) {
+			// Préparation pour insertion de la 1ère ligne
+			if (*tab_commande == NULL) {
+				*tab_commande = (type_ligne_commande *)malloc(sizeof(type_ligne_commande) * nb_produits);
+				*derniere_ligne = *tab_commande;
+			}
+			// Préparation pour insertion des autres lignes
+			else {
+				(*derniere_ligne)++;
+			}
+			(*derniere_ligne)->ptr_produit = nv_ptr_produit;
+			(*derniere_ligne)->quantite = quantite;
+			(*derniere_ligne)->total_ligne = quantite * nv_ptr_produit->prix_unitaire;
+			*total = *total + (*derniere_ligne)->total_ligne;
+
+			//Affichage de la nouvelle ligne de commande
+			Afficher_Ligne_Commande(*derniere_ligne);
+		}
+		else {
+			puts("La quantite saisie ne peut etre <= 0");
+		}
 	}
 }
 
@@ -432,17 +475,22 @@ void Ajout_Ligne(type_ligne_commande **tab_commande, type_ligne_commande **derni
 // - empecher quantite negatives
 void Modif_Ligne(int nv_quantite, type_ligne_commande *ligne_commande, float *total) {
 
-	if (nv_quantite + ligne_commande->quantite > 0) {
-		ligne_commande->quantite = nv_quantite + ligne_commande->quantite;
-		ligne_commande->total_ligne = ligne_commande->quantite * ligne_commande->ptr_produit->prix_unitaire;
-
-		*total = *total + nv_quantite * ligne_commande->ptr_produit->prix_unitaire;
-
-		//Affichage de la ligne de commande modifiée
-		Afficher_Ligne_Commande(ligne_commande);
+	if (ligne_commande == NULL) {
+		puts("Erreur : la ligne est indéfinie");
 	}
 	else {
-		puts("Il n'est pas possible de commander une quantite inferieure a 1");
+		if (nv_quantite + ligne_commande->quantite > 0) {
+			ligne_commande->quantite = nv_quantite + ligne_commande->quantite;
+			ligne_commande->total_ligne = ligne_commande->quantite * ligne_commande->ptr_produit->prix_unitaire;
+
+			*total = *total + nv_quantite * ligne_commande->ptr_produit->prix_unitaire;
+
+			//Affichage de la ligne de commande modifiée
+			Afficher_Ligne_Commande(ligne_commande);
+		}
+		else {
+			puts("Il n'est pas possible de commander une quantite inferieure a 1");
+		}
 	}
 }
 
@@ -453,7 +501,7 @@ void Modif_Ligne(int nv_quantite, type_ligne_commande *ligne_commande, float *to
 // - ligne commande vide
 void Supprimer_ligne(type_ligne_commande **tab_commande, type_ligne_commande *adresse_commande, type_ligne_commande **derniere_ligne, float *total) {
 
-	if (*tab_commande != NULL) {
+	if (*tab_commande != NULL && adresse_commande != NULL && *derniere_ligne != NULL) {
 
 		*total = *total - (adresse_commande->total_ligne);
 
@@ -471,6 +519,9 @@ void Supprimer_ligne(type_ligne_commande **tab_commande, type_ligne_commande *ad
 		else {
 			(*derniere_ligne)--;
 		}
+	}
+	else {
+		puts("Erreur d'adressage");
 	}
 }
 
@@ -501,8 +552,14 @@ void Commande_Produit(type_produit *tab_produit, type_ligne_commande **tab_comma
 		adresse_commande = Recherche_Ligne(no_produit, *tab_commande, *derniere_ligne);
 		// Si non, on ajoute une ligne
 		if (adresse_commande == NULL) {
-			nb_produits = dernier_produit - tab_produit + 1;
-			Ajout_Ligne(tab_commande, derniere_ligne, nb_produits, adresse_produit, quantite, total);
+			if (dernier_produit == NULL || tab_produit == NULL) {
+				puts("Erreur d'adressage");
+			}
+			else {
+				// On calcule le nb de produits en fonction des adresses du premier et du dernier produit
+				nb_produits = dernier_produit - tab_produit + 1;
+				Ajout_Ligne(tab_commande, derniere_ligne, nb_produits, adresse_produit, quantite, total);
+			}
 		}
 		// Si oui, on modifie ou on supprime une ligne selon la quantité
 		else {
@@ -518,37 +575,45 @@ void Commande_Produit(type_produit *tab_produit, type_ligne_commande **tab_comma
 
 // Génération du fichier HTML de la facture
 // Tests
-// - fichier existe, pas de droits
 // - pas possible d'enregistrer modif
 // - Facture vide
+// - fichier existant
+// - pas de droits
+// - fichier ouvert dans un autre programme
+// - caractères interdits nom du fichier
+// - nom de fichier trop long
 void Creation_Facture(char *nom, char *prenom, float total, type_ligne_commande *tab_commande, type_ligne_commande *derniere_ligne) {
 	FILE *fichier_facture;
 	char nom_complet[MAX_CHAINE];
 
-	// On nomme le fichier d'après le nom et prénom de la personne
-	strcpy(nom_complet, nom);
-	strcat(nom_complet, prenom);
-	fichier_facture = fopen(strcat(nom_complet, ".html"), "w");
+	if (tab_commande == NULL || derniere_ligne == NULL) {
+		puts("Erreur d'adressage");
+	} {
+		// On nomme le fichier d'après le nom et prénom de la personne
+		strcpy(nom_complet, nom);
+		strcat(nom_complet, prenom);
+		fichier_facture = fopen(strcat(nom_complet, ".html"), "w");
 
-	if (fichier_facture == NULL) {
-		puts("Impossible de créer le fichier");
-	}
-	else {
-		fputs("<html>\n<head>\n<title>Facture</title>\n</head>\n<body>\n", fichier_facture);
-		fprintf(fichier_facture, "<h1>Facture de %s %s</h1>\n", nom, prenom);
-		fputs("<table border>\n<tr bgcolor=\"yellow\">", fichier_facture);
-		fputs("<td>No</td><td>Marque</td><td>Ref</td><td>Prix</td><td>Nb</td><td>Total</td></tr>\n", fichier_facture);
-		// On imprime chaque ligne de commande
-		while (tab_commande <= derniere_ligne) {
-			fprintf(fichier_facture, "<tr><td>%d</td><td>%s</td><td>%s</td><td>%.2f</td><td>%d</td><td align=\"right\">%.2f</td></tr>\n", tab_commande->ptr_produit->no, tab_commande->ptr_produit->marque, tab_commande->ptr_produit->ref, tab_commande->ptr_produit->prix_unitaire, tab_commande->quantite, tab_commande->total_ligne);
-			tab_commande++;
+		if (fichier_facture == NULL) {
+			puts("Impossible de creer le fichier");
 		}
-		// On imprime le total de la commande
-		fprintf(fichier_facture, "<tr><td>Total</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td align=\"right\">%.2f</td></tr>\n", total);
-		fputs("</table>\n</body>\n</html>", fichier_facture);
+		else {
+			fputs("<html>\n<head>\n<title>Facture</title>\n</head>\n<body>\n", fichier_facture);
+			fprintf(fichier_facture, "<h1>Facture de %s %s</h1>\n", nom, prenom);
+			fputs("<table border>\n<tr bgcolor=\"yellow\">", fichier_facture);
+			fputs("<td>No</td><td>Marque</td><td>Ref</td><td>Prix</td><td>Nb</td><td>Total</td></tr>\n", fichier_facture);
+			// On imprime chaque ligne de commande
+			while (tab_commande <= derniere_ligne) {
+				fprintf(fichier_facture, "<tr><td>%d</td><td>%s</td><td>%s</td><td>%.2f</td><td>%d</td><td align=\"right\">%.2f</td></tr>\n", tab_commande->ptr_produit->no, tab_commande->ptr_produit->marque, tab_commande->ptr_produit->ref, tab_commande->ptr_produit->prix_unitaire, tab_commande->quantite, tab_commande->total_ligne);
+				tab_commande++;
+			}
+			// On imprime le total de la commande
+			fprintf(fichier_facture, "<tr><td>Total</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td align=\"right\">%.2f</td></tr>\n", total);
+			fputs("</table>\n</body>\n</html>", fichier_facture);
 
-		if (fclose(fichier_facture) == EOF) {
-			puts("Erreur de fermeture du fichier");
+			if (fclose(fichier_facture) == EOF) {
+				puts("Erreur de fermeture du fichier");
+			}
 		}
 	}
 }
